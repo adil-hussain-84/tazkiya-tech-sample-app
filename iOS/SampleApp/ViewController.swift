1a9b38332fb396f91c2fd90a21b7ba07853e7624
Adil Hussain <>
2020-09-25 11:57:13 +0100

    (iOS) Implemented the `ViewController.onOpenHifdhRevisionTrackerAppButtonTapped(...)` to open the Hifdh Revision Tracker app if it's installed on the device and otherwise to open the app's landing page in App Store.

--- iOS/SampleApp/ViewController.swift
+++ iOS/SampleApp/ViewController.swift
@@ -11,6 +11,11 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBAction func onOpenHifdhRevisionTrackerAppButtonTapped(_ sender: Any) {
-        // TODO
+        
+        if let appURL = URL(string: "HifdhRevisionTracker:"), UIApplication.shared.canOpenURL(appURL) {
+            UIApplication.shared.open(appURL)
+        } else if let appStoreURL = URL(string: "https://apps.apple.com/app/id1524002702"), UIApplication.shared.canOpenURL(appStoreURL) {
+            UIApplication.shared.open(appStoreURL)
+        }
     }
 }
